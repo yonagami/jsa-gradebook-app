e3ab7079b6e6fe1d40762967968e1be582676b30
Yonagami <yonagami@yonagami.blahblahblah>
2025-05-30 00:35:06 +0200

    Step 3

---
+++ script3.js
@@ -0,0 +1,41 @@
+function getAverage(scores) {
+  let sum = 0;
+
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+function hasPassingGrade(score) {
+  let grade = getGrade(score);
+  if (grade != "F") {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));
+
+
